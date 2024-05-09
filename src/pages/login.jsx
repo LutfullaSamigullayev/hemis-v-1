0ac4b46a3dfe8677c8ec2e443149562f8452d047
Your Name <you@example.com>
2024-05-10 01:03:14 +0500

    add login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,3 +1,52 @@
+import { Link } from "react-router-dom";
+import { Icons, Button } from "../components";
 export function Login() {
-  return <div className="login">Login</div>;
+  return (
+    <div className="register login">
+      <Link to="/" className="logo absolute">
+        <img src="/header/logo.png" alt="logo" />
+      </Link>
+      <div className="register__wrapper">
+        <h1>REGISTER</h1>
+        <form action="" id="form" className="register__form">
+          <label htmlFor="username">Username</label>
+          <div className="input">
+            <div className="svg">
+              <Icons.login />
+            </div>
+            <input
+              type="text"
+              placeholder="Enter Your Email"
+              name="Username"
+              id="Username"
+              required
+            />
+          </div>
+          <label htmlFor="username">Email</label>
+          <input
+            type="text"
+            placeholder="Enter Your Email"
+            name="Username"
+            id="Username"
+            required
+          />
+          <div className="submit">
+            <Link to="/signUp">New Account ??</Link>
+            <Button>LOGIN</Button>
+          </div>
+          <div className="register__icons">
+            <div>
+              <Icons.google />
+            </div>
+            <div>
+              <Icons.apple />
+            </div>
+            <div>
+              <Icons.facebook2 />
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
--- src/pages/signUp.jsx
+++ src/pages/signUp.jsx
@@ -1,14 +1,61 @@
+import { Link } from "react-router-dom";
+import { Button, Icons } from "../components";
 export function SignUp() {
   return (
-    <div className="signUp">
-      <div className="signUp__wrapper">
+    <div className="register signUp">
+      <Link to="/" className="logo absolute">
+        <img src="/header/logo.png" alt="logo" />
+      </Link>
+      <div className="register__wrapper">
         <h1>REGISTER</h1>
-        
-        <form action="" id="form">
+        <form action="" id="form" className="register__form">
           <label htmlFor="username">Username</label>
-          <input type="text" placeholder="Enter Username" name="Username" id="Username" required />
-          <label htmlFor="username">Username</label>
-          <input type="text" placeholder="Enter Username" name="Username" id="Username" required />
+          <input
+            type="text"
+            placeholder="Enter Your Username"
+            name="Username"
+            id="Username"
+            required
+          />
+          <label htmlFor="username">Email</label>
+          <input
+            type="text"
+            placeholder="Enter Your Email"
+            name="Username"
+            id="Username"
+            required
+          />
+          <label htmlFor="username">Phone</label>
+          <input
+            type="text"
+            placeholder="Enter Your Phone number"
+            name="Username"
+            id="Username"
+            required
+          />
+          <label htmlFor="username">Password</label>
+          <input
+            type="Password"
+            placeholder="Enter Password"
+            name="Username"
+            id="Username"
+            required
+          />
+          <div className="submit">
+            <Link to="/login">Login ??</Link>
+            <Button>REGISTER</Button>
+          </div>
+          <div className="register__icons">
+            <div>
+              <Icons.google />
+            </div>
+            <div>
+              <Icons.apple />
+            </div>
+            <div>
+              <Icons.facebook2 />
+            </div>
+          </div>
         </form>
       </div>
     </div>
