e99269bc66808aa8683a60966601e1dec0e7d972
Your Name <you@example.com>
2024-05-09 18:03:55 +0500

    add signUp

--- src/pages/signUp.jsx
+++ src/pages/signUp.jsx
@@ -1,3 +1,16 @@
 export function SignUp() {
-  return <div className="register">Register</div>;
+  return (
+    <div className="signUp">
+      <div className="signUp__wrapper">
+        <h1>REGISTER</h1>
+        
+        <form action="" id="form">
+          <label htmlFor="username">Username</label>
+          <input type="text" placeholder="Enter Username" name="Username" id="Username" required />
+          <label htmlFor="username">Username</label>
+          <input type="text" placeholder="Enter Username" name="Username" id="Username" required />
+        </form>
+      </div>
+    </div>
+  );
 }
